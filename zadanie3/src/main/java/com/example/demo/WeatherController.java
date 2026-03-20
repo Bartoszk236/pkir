7af4b5df480fcfb7144aec1031a158999413112e
bartoszkocylo <bartoszk236@gmail.com>
2026-03-20 18:47:58 +0100

    Zadanie 1 - Dodanie Swaggera

--- zadanie3/src/main/java/com/example/demo/WeatherController.java
+++ zadanie3/src/main/java/com/example/demo/WeatherController.java
@@ -1,14 +1,16 @@
 package com.example.demo;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class WeatherController {
     private final OpenWeatherClient openWeatherClient;
+    private final List<String> cities = new ArrayList<>();
 
     @GetMapping("/weather/forecast")
     public ForecastResponse forecast(@RequestParam String city) {
@@ -16,5 +18,23 @@ public class WeatherController {
             throw new IllegalArgumentException("Parameter 'city' is required");
         }
         return openWeatherClient.getForecastByCity(city.trim());
+    }
+
+
+    @PostMapping("/cities")
+    public String addCity(@RequestBody CityRequest request) {
+        if (request == null || request.name() == null || request.name().isBlank()) {
+            throw new IllegalArgumentException("City name cannot be empty");
+        }
+
+        String cityName = request.name().trim();
+        cities.add(cityName);
+
+        return "City was added: " + cityName;
+    }
+
+    @GetMapping("/cities")
+    public List<String> getCities() {
+        return cities;
     }
 }
